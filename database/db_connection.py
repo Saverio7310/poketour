611f4671c74c4c691c30aca1de22f52b93afb0fa
Saverio Perrone <saverio.perrone20@gmail.com>
2023-02-16 13:25:06 +0100

    inserimenti funzionanti (non insert_pokemon)

--- database/db_connection.py
+++ database/db_connection.py
@@ -11,18 +11,21 @@ class DBConnection():
         connection = sqlite3.connect('Database/tournament.db')
         return connection
     
-    def exec_tables_creation(self, conn, cursor):
-        result = conn.execute("""
-        CREATE TABLE prova(
-        link text,
-        position integer
-        )
-        """)
-        return result
-    
     def create_table_new_tournament(self, conn, tour_code):
-        statement1 = 'CREATE TABLE IF NOT EXISTS ?(link text PRIMARY KEY, standing integer)'.replace('?', tour_code)
+        statement1 = """
+        CREATE TABLE IF NOT EXISTS tournaments(
+        name text PRIMARY KEY, 
+        link text UNIQUE
+        )
+        """
         statement2 = """
+        CREATE TABLE IF NOT EXISTS ?(
+        link text PRIMARY KEY, 
+        standing integer, 
+        FOREIGN KEY(link) REFERENCES tournaments ON DELETE CASCADE ON UPDATE CASCADE
+        )
+        """.replace('?', tour_code)
+        statement3 = """
         CREATE TABLE IF NOT EXISTS teams_?(
         link text,
         name text,
@@ -35,14 +38,37 @@ class DBConnection():
         moved text,
         typea text,
         typeb text,
-        PRIMARY KEY(link, name)
+        PRIMARY KEY(link, name),
+        FOREIGN KEY(link) REFERENCES ? ON DELETE CASCADE ON UPDATE CASCADE
         )
         """.replace('?', tour_code[:20])
         conn.execute(statement1)
         conn.execute(statement2)
+        conn.execute(statement3)
+        conn.commit()
+
+    def insert_tournament(self, conn, tour_name, tour_code):
+        statement = "INSERT OR IGNORE INTO tournaments VALUES (?, ?)"
+        result = conn.execute(statement, (tour_name, tour_code)).fetchall()
+        conn.commit()
+        return result
+
+    def insert_links(self, conn, tour_code, list_of_tuples):
+        statement = "INSERT OR IGNORE INTO ! (link, standing) VALUES (?, ?)".replace('!', tour_code)
+        conn.executemany(statement, list_of_tuples)
+        conn.commit()
 
     def insert_pokemons(self, conn, tour_code, list_of_tuples):
-        statement = "INSERT INTO teams_! VALUES (?)".replace('!', tour_code)
+        '''
+        INSERT OR IGNORE INTO teams_h7kIYruNMePQMy4UZkMj 
+        (link, name, ability, tera, item, movea, moveb, movec, moved) 
+        VALUES ('azVMOBBztqFsyv6Oe03m', 'gholdengo', 'intimidate', 'steel', 'choice specs', 'protect', 'protect', 'protect', 'protect')
+        '''
+        statement = "INSERT OR IGNORE INTO teams_! (link, name, ability, tera, item, movea, moveb, movec, moved) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)".replace('!', tour_code)
         conn.executemany(statement, list_of_tuples)
+        conn.commit()
+
+    def close_connection(self, conn):
+        conn.close()
         
 
