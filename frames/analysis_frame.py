b18aae7f5462f7d49b3733dded2a053e0fda9528
Saverio Perrone <saverio.perrone20@gmail.com>
2023-02-20 19:32:50 +0100

    agg lista di bottoni che aprono finestra su torneo

--- frames/analysis_frame.py
+++ frames/analysis_frame.py
@@ -1,9 +1,25 @@
 import customtkinter
+from functools import partial
+import Frames.tournament_window as tw
+from Database.db_connection import DBConnection
 
 class AnalysisFrame(customtkinter.CTkScrollableFrame):
     def __init__(self, master, **kwargs):
         super().__init__(master, **kwargs)
-        
-        for i in range(20):
-            self.btt = customtkinter.CTkButton(self.second_frame, text= str(i))
-            self.btt.grid(row=i, column=0, padx=10, pady=10, sticky="nsew")
+
+        db = DBConnection()
+        conn = db.start_connection()
+        result = db.fetch_tournaments_data(conn)
+        db.close_connection(conn)
+
+        for i, tup in enumerate(result):
+            btt = customtkinter.CTkButton(self, text= tup[0] + ', codice: ' + tup[1], command=partial(self.open_toplevel, tup[0], tup[1]))
+            btt.grid(row=i, column=0, padx=10, pady=10, sticky="nsew")
+
+        self.toplevel_window = None
+
+    def open_toplevel(self, name, code):
+        if self.toplevel_window is None or not self.toplevel_window.winfo_exists():
+            self.toplevel_window = tw.TournamentWindow(self, tour_name=name, tour_code=code)  # create window if its None or destroyed
+        else:
+            self.toplevel_window.focus()  # if window exists focus it
