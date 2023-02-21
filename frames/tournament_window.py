29978a524ed52d23d3ff777d50c6678a25c9769a
Saverio Perrone <saverio.perrone20@gmail.com>
2023-02-21 20:13:12 +0100

    agg style, dati, click event in treeview

--- frames/tournament_window.py
+++ frames/tournament_window.py
@@ -1,4 +1,5 @@
 import customtkinter
+import threading
 import tkinter
 from tkinter import ttk
 from Database.db_connection import DBConnection
@@ -10,41 +11,142 @@ class TournamentWindow(customtkinter.CTkToplevel):
         self.geometry("800x600")
         self.title('Analisi ' + str(tour_name))
 
-        self.label_name = customtkinter.CTkLabel(self, text=tour_name)
-        self.label_name.grid(padx=20, pady=20)
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_rowconfigure(0, weight=1)
 
         db = DBConnection()
         conn = db.start_connection()
         participants, top_poke, top_tera = db.get_general_info(conn, tour_code)
 
+        dict_poke_info = dict()
+
+        def get_movesets(top_moveset):
+            moveset_list = list()
+
+            for moveset in top_moveset:
+                moveset_list.append(set(moveset))
+
+            moveset_list_final = list()
+
+            for move_set1 in moveset_list:
+                num = 0
+                for move_set2 in moveset_list:
+                    if move_set2 == move_set1:
+                        num += 1
+                        moveset_list.remove(move_set2)
+                moveset_list_final.append((move_set1, num))
+
+            moveset_list_final.sort(key=lambda a : a[1], reverse=True)
+            return moveset_list_final
+
+        def get_data():
+            db = DBConnection()
+            conn = db.start_connection()
+            for poke, _ in top_poke:
+                moveset, tera, ability, item = db.get_specific_poke_info(conn, tour_code, poke)
+                movesets = get_movesets(moveset)
+                dict_poke_info[poke] = (movesets, tera, ability, item)
+
+
+        threading.Thread(target=get_data).start()
+
         self.frame = customtkinter.CTkFrame(self, corner_radius=10)
-        self.frame.grid(row=0, column=0, sticky='nsew')
-        self.frame.grid_rowconfigure(0, weight=1)
-        self.frame.grid_columnconfigure(0, weight=1)
+        self.frame.grid(row=0, column=0, columnspan = 2, sticky='nsew', padx=10, pady=10)
+        self.frame.grid_columnconfigure((0, 1), weight=1)
+        self.frame.grid_rowconfigure((0, 2), weight=1)
 
-        columns = ('name', 'usage')
+        self.label_name = customtkinter.CTkLabel(self.frame, text=tour_name)
+        self.label_name.cget("font").configure(size=50)
+        self.label_name.grid(row=0, column=0, columnspan=2, sticky='nsew')
 
-        self.tree = ttk.Treeview(self.frame, columns=columns, show='headings')
-        self.tree.grid(row=1, column=0, sticky='nsew')
+        str_to_show = 'Dati generati su un totale di ? partecipanti'.replace('?', str(participants[0]))
+        self.label_part = customtkinter.CTkLabel(self.frame, text=str_to_show, anchor='sw')
+        #self.label_part.cget("font").configure(size=50)
+        self.label_part.grid(row=1, column=0, columnspan=2, sticky='nsew')
 
-        self.tree.heading('name', text='Name')
-        self.tree.heading('usage', text='Usage')
 
-        for tup in top_poke:
-            self.tree.insert('', tkinter.END, values=tup)
-
-        self.label_part = customtkinter.CTkLabel(self.frame, text=participants)
-        self.label_part.grid(row=0, column=0, sticky='nsew')
+        # Add some style
+        style = ttk.Style()
+        #Pick a theme
+        style.theme_use("default")
 
         '''
+        # Configure our treeview colors
+        style.configure("Treeview", 
+            background="#D3D3D3",
+            foreground="black",
+            rowheight=25,
+            fieldbackground="#D3D3D3"
+            )
+        # Change selected color
+        style.map('Treeview', background=[('selected', 'blue')])
+        '''
+
+
+        # Configure our treeview colors
+        style.configure(
+            "Treeview",
+            background="#333333",
+            foreground="#DCE4EE",
+            fieldbackground="#333333",
+            rowheight=30,
+            )
+        style.configure(
+            "Treeview.Heading",
+            background="#333333",
+            foreground="#DCE4EE",
+            #relief="flat",
+            rowheight=30,
+            font=(None, 20)
+            )
+        # Change selected color
+        style.map('Treeview', background=[('selected', '#3B8ED0')])
+        style.map('Treeview.Heading', background=[('selected', '#3B8ED0')])
+
+        self.top_poke_tree = ttk.Treeview(self.frame, show='headings')
+        self.top_poke_tree.grid(row=2, column=0, sticky='nsew')
+        self.top_poke_tree['columns'] = ('name', 'usage')
+        self.top_poke_tree.column('name', minwidth=160)
+        self.top_poke_tree.column('usage', width=50, minwidth=40)
+
+        self.top_poke_tree.heading('name', text='Name', anchor=tkinter.W)
+        self.top_poke_tree.heading('usage', text='Usage', anchor=tkinter.W)
+
+
         for tup in top_poke:
-            self.label_top_poke = customtkinter.CTkLabel(self.frame, text=tup)
-            self.label_top_poke.grid(padx=20, pady=20)
+            self.top_poke_tree.insert('', tkinter.END, values=tup)
+
+        self.info_poke_frame = customtkinter.CTkFrame(self, corner_radius=10)
+        #self.info_poke_frame.grid_columnconfigure((0, 1), weight=1)
+        #self.info_poke_frame.grid_rowconfigure((0, 2), weight=1)
+        
+        def click(e):
+            # Grab record number
+            selected = self.top_poke_tree.focus()
+            # Grab record values
+            values = self.top_poke_tree.item(selected, 'values')
+            moveset, tera, ability, item = dict_poke_info[values[0]]
+            self.info_poke_frame.grid(row=3, column=0, columnspan = 2, sticky='sew', padx=10, pady=10)
+            #self.info_poke_frame.grid_propagate(0)
+            #self.info_poke_frame.configure(width=300)
+            self.label_t = customtkinter.CTkLabel(self.info_poke_frame, text=tera[0][0])
+            self.label_t.cget("font").configure(size=20)
+            self.label_t.grid(row=0, column=0, columnspan=2, sticky='nsew')
+            print(moveset)
+            print(tera)
+            print(ability)
+            print(item)
+
+        self.top_poke_tree.bind('<Double-1>', click)
+
+        self.top_tera_tree = ttk.Treeview(self.frame, show='headings')
+        self.top_tera_tree.grid(row=2, column=1, sticky='nsew')
+        self.top_tera_tree['columns'] = ('tera', 'usage')
+        self.top_tera_tree.column('tera', minwidth=160)
+        self.top_tera_tree.column('usage', width=50, minwidth=40)
+
+        self.top_tera_tree.heading('tera', text='Teratype', anchor=tkinter.W)
+        self.top_tera_tree.heading('usage', text='Usage', anchor=tkinter.W)
 
         for tup in top_tera:
-            self.label_top_tera = customtkinter.CTkLabel(self.frame, text=tup)
-            self.label_top_tera.grid(padx=20, pady=20)
-        '''
-
-
-
+            self.top_tera_tree.insert('', tkinter.END, values=tup)
