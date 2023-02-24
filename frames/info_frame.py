b9fcc9db6edc07528ef361b57cc7a475876c3231
Saverio Perrone <saverio.perrone20@gmail.com>
2023-02-24 13:27:02 +0100

    agg commenti

--- frames/info_frame.py
+++ frames/info_frame.py
@@ -5,9 +5,11 @@ class InfoFrame(customtkinter.CTkScrollableFrame):
     def __init__(self, master, name, moveset, tera, ability, item, **kwargs):
         super().__init__(master, **kwargs)
 
+        # configurazione della pagina
         self.grid_columnconfigure((0, 1, 2), weight=1)
         self.grid_rowconfigure((0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13), weight=1)
 
+        # creazione dei label relativi ai dati specifici del pokemon cliccato
         self.label_name = customtkinter.CTkLabel(self, text=name, anchor='center')
         self.label_name.cget("font").configure(size=20)
         self.label_name.grid(row=0, column=1, sticky='nsew', pady = (10, 30))
@@ -24,7 +26,7 @@ class InfoFrame(customtkinter.CTkScrollableFrame):
         self.label_values.cget("font").configure(size=20)
         self.label_values.grid(row=1, column=2, sticky='nsew', pady = (10, 30))
 
-        # moveset
+        # riga deidicata ai moveset
         self.label_moveset = customtkinter.CTkLabel(self, text='Moveset')
         self.label_moveset.cget("font").configure(size=20)
 
@@ -40,6 +42,7 @@ class InfoFrame(customtkinter.CTkScrollableFrame):
             i += 1
 
         last = i
+        # serve a capire dove inserire la label che indica i moveset
         if last-start == 1:
             self.label_moveset.grid(row=i-1, column=0, sticky='nsew')
         elif last-start > 1:
@@ -52,12 +55,11 @@ class InfoFrame(customtkinter.CTkScrollableFrame):
         i += 1
         start = i
 
-        # tera
+        # riga deidicata ai teratipi
         self.label_tera = customtkinter.CTkLabel(self, text='Teratipo')
         self.label_tera.cget("font").configure(size=20)
 
         for teratype, usage in tera[:3]:
-            #string = ', '.join(moves)
             label = customtkinter.CTkLabel(self, text=teratype)
             label.grid(row=i, column=1, sticky='nsew')
             label = customtkinter.CTkLabel(self, text=usage)
@@ -77,12 +79,11 @@ class InfoFrame(customtkinter.CTkScrollableFrame):
         i += 1
         start = i
 
-        # ability
+        # riga deidicata alle abilità
         self.label_ability = customtkinter.CTkLabel(self, text='Abilità')
         self.label_ability.cget("font").configure(size=20)
 
         for abil, usage in ability[:3]:
-            #string = ', '.join(moves)
             label = customtkinter.CTkLabel(self, text=abil)
             label.grid(row=i, column=1, sticky='nsew')
             label = customtkinter.CTkLabel(self, text=usage)
@@ -102,7 +103,7 @@ class InfoFrame(customtkinter.CTkScrollableFrame):
         i += 1
         start = i
 
-        # item
+        # riga deidicata agli item
         self.label_item = customtkinter.CTkLabel(self, text='Oggetto')
         self.label_item.cget("font").configure(size=20)
 
@@ -119,32 +120,3 @@ class InfoFrame(customtkinter.CTkScrollableFrame):
             self.label_item.grid(row=i-1, column=0, sticky='nsew')
         elif last-start > 1:
             self.label_item.grid(row=i-2, column=0, sticky='nsew')
-
-        """ self.label_name = customtkinter.CTkLabel(self, text=moveset[0][0])
-        self.label_name.cget("font").configure(size=20)
-        self.label_name.grid(row=2, column=1, sticky='nsew')
-
-        self.label_name = customtkinter.CTkLabel(self, text=moveset[1][0])
-        self.label_name.cget("font").configure(size=20)
-        self.label_name.grid(row=3, column=1, sticky='nsew')
-
-        self.label_name = customtkinter.CTkLabel(self, text=moveset[2][0])
-        self.label_name.cget("font").configure(size=20)
-        self.label_name.grid(row=4, column=1, sticky='nsew') """
-
-        """ # tera
-        self.label_name = customtkinter.CTkLabel(self, text=name)
-        self.label_name.cget("font").configure(size=20)
-        self.label_name.grid(row=0, column=0, columnspan=3, sticky='nsew')
-
-        self.label_name = customtkinter.CTkLabel(self, text='Info')
-        self.label_name.cget("font").configure(size=20)
-        self.label_name.grid(row=1, column=0, sticky='nsew')
-
-        self.label_name = customtkinter.CTkLabel(self, text='Dati')
-        self.label_name.cget("font").configure(size=20)
-        self.label_name.grid(row=1, column=1, sticky='nsew')
-
-        self.label_name = customtkinter.CTkLabel(self, text='Valori')
-        self.label_name.cget("font").configure(size=20)
-        self.label_name.grid(row=1, column=2, sticky='nsew') """
