1c7400fd0ab0e6e709b52b98a2799bb0c4a18735
Saverio Perrone <saverio.perrone20@gmail.com>
2023-02-24 13:25:18 +0100

    agg commenti + rimozione due funzioni inutili

--- classes/download.py
+++ classes/download.py
@@ -18,6 +18,7 @@ class Download():
     TEAMLIST_URL = "https://rk9.gg"
     TOURNAMENT_CODE = "df5AzRjKxTb62H7BsbOe"
 
+    # header della chiamata http che serve a farsi sgamare di meno dai siti durante le connessioni
     header = {
         'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.2 Safari/605.1.15',
         'Accept-Language':'en-GB,en;q=0.5',
@@ -25,6 +26,7 @@ class Download():
         'DNT':'1'
     }
 
+    # connessione alla pagina rk9 del torneo specificato
     def establish_connection(self, code):
         # driver per andare a caricare la pagina html dato che la lista non viene caricata tutta subito. Non so nello specifico
         # a cosa servino. In questo caso ho usato ChromeOpitons dato che è uno dei browser più usati.
@@ -49,35 +51,12 @@ class Download():
         soup = bs4.BeautifulSoup(web_driver.page_source, "html.parser")
         return soup.find(id="dtLiveRoster")
 
-    # i due metodi seguenti servono a prendere i dati dei team. Se li si prende prima che i 
-    # risultati con le posizioni siano usciti allora non bisogna prendere in considerazione la 
-    # posizione perché ovviamente non c'è. Questa scelta la prendo attraverso un try - except
-    def __get_link_with_standing(tokens, items, set_teams):
-        pass
-
-    def __get_link_without_standing(tokens, items, set_teams):
-        '''
-        for item_table_row in table_rows:
-                tokens_list_row = item_table_row.text.split()
-                print(*tokens_list_row, sep="\n")
-                #print(type(tokens_ex), len(tokens_ex))
-                #self.__get_link_without_standing(tokens_list_row, item_table_row, set_teams)
-                if 'Masters' in tokens_list_row:
-                    for item_anchor in item_table_row.find_all("a"):
-                        link = item_anchor['href']
-                    set_teams.add((link,))
-                    print('inserisco:', link)
-                # trasformo il set in una lista e printo la linghezza per capire quanti elementi contiene
-                list_teams = list(set_teams)
-                print(len(list_teams)) 
-        '''
-
     def get_teams_link(self, table):
         # scandisco riga per riga "tr => table row" e prendo tutti i dati testuali contenuti. Splitto parola per parola tramite
         # l'attributo .text e prendo in considerazione solo i Masters. Per ognuno di loro salvo il link del team, contenuto nel
-        # tag "a => anchor text for hyperlinking", in un set assieme alla posizione in classifica (che serve solo se voglio
-        # analizzare la top 32 altrimenti non serve) ottenuta tramite l'indice successivo all'elemento 'View'. Infine salvo nel
-        # set la tupla (link, posizione)
+        # tag "a => anchor text for hyperlinking", in un set assieme alla posizione in classifica (nel caso non siano state 
+        # pubblicate le posizioni inserisco un placeholder '-1') ottenuta tramite l'indice successivo all'elemento 'View'. 
+        # Infine salvo nel set la tupla (link, posizione)
 
         # set che andrà a contenere tutti i link dei team partecipanti
         set_teams = set()
@@ -101,6 +80,12 @@ class Download():
         print(len(set_teams))
         return set_teams
     
+    # una volta salvati i link, posso collegarmi alle pagine dei team tramite una nuova connessione. Non scandisco sui link
+    # presenti nell'insieme ritornato dalla funzione precedente perché questa funzione la chiamo tramite Thread, quindi
+    # i link li passo con quella chiamta e qui il codice dev funzionare sul singolo link. È possibile che per le troppe 
+    # connessioni il sito blocchi la connessione al mio indirizzo IP, generando 3 eccezioni una dentro l'altra. Per questo
+    # uso un try except usando un generico 'Exception' dato che non saprei quale scegliere delle 3+ che vengono lanciate. 
+    # Per velocizzare il tutto, questa funzione ritorna tutto il codice html che viene lavorato trmaite un'altra funzione.
     def get_table_html(self, team_link):
         try:
             with requests.Session() as s:
@@ -111,20 +96,21 @@ class Download():
             raise Exception
         
     def get_all_teams(self, table_link_tuple):
-        # itero sul set di link per andare ad analizzare i dati di tutti i team. Il link in realtà è solo parziale, quindi devo
-        # completarlo. Una volta fatto ciò possono iniziare a collegarmi ad ognuna delle pagine in questione tramite bs4. Cerco
-        # la classe in questione che contiene i pokemon in lingua inglese e poi quella che contiene ogni singolo pokemon. Prima 
-        # di tutto cerco le mosse tramite la loro classe e le inserisco in una lista da cui creo una stringa unica che mi 
-        # servirà dopo per trovare l'oggetto. Fatto ciò, prendo i dati tramite un .text e creo un stringa unica da cui vado ad
-        # eliminare eventuali soprannomi. Dopo splitto la stringa tramite regex per andare ad ottenere tutti i dati necessari 
-        # all'interno di una lista. Per raccogliere nome, tera ed abilità accedo semplicemente alle prime 3 posizioni. Per trovare
-        # l'oggetto la situazione di complica. Dato che il sito è fatto bene bene, tramite il .text si prendono anche le mosse 
-        # oltre a tutte le altre informazioni. Per togliere le mosse dall'elemento della lista contenete l'oggetto uso la stringa
+        # itero sul set di link per andare ad analizzare i dati di tutti i team. Il link in realtà è solo parziale, 
+        # quindi devo completarlo. Fatto ciò, mi collego ad ogni pagina tramite bs4. Cerco la classe che contiene i
+        #  pokemon in lingua inglese e poi quella che contiene ogni singolo pokemon. Per prima cosa 
+        # cerco le mosse tramite la loro classe e le inserisco in una lista da cui creo una stringa unica che mi 
+        # servirà dopo per trovare l'item. Poi prendo i dati tramite un .text e creo un stringa unica da cui vado ad
+        # eliminare eventuali soprannomi ed altre parti della stringa che non mi servono. Dopo splitto la stringa 
+        # tramite regex per andare ad ottenere tutti i dati necessari all'interno di una lista. Per raccogliere nome, 
+        # tera ed abilità accedo semplicemente alle prime 3 posizioni. Per trovare l'oggetto il procedimento complica.
+        #  Dato che il sito è fatto bene bene, tramite il .text si prendono anche le mosse oltre a tutte le altre
+        #  informazioni. Per togliere le mosse dall'elemento della lista contenete l'oggetto uso la stringa
         # di prima con le mosse unificate. In questo modo, dato che la stringa finale è composta da oggetto + lista mosse, 
-        # sottraendo da questa la stringa delle mosse rimango con l'oggetto. L'oggetto risultante è formato da una singola parola.
-        # Uso quindi un piccolo ciclo con cui trasformo la stringa in una lista di caratteri ed inserisco una spazio vuoto prima
-        # di ogni lettera maiuscola. Fatto ciò, ho raccolto tutti i dati che mi servivano e posso inserire il pokemon nel suo
-        # team e il team nella lista di tutti i team.
+        # sottraendo da questa la stringa delle mosse rimango con l'oggetto. L'oggetto risultante è formato da una singola
+        # parola. Uso quindi un piccolo ciclo con cui trasformo la stringa in una lista di caratteri ed inserisco una 
+        # spazio vuoto prima di ogni lettera maiuscola. Fatto ciò, ho raccolto tutti i dati che mi servivano e posso 
+        # inserire il pokemon nel suo team e il team nella lista di tutti i team.
         list_tuple_all_poke = list()
 
         for single_pokemon in table_link_tuple[0].find_all("div", {"class": "pokemon bg-light-green-50 p-3"}):
@@ -169,6 +155,3 @@ class Download():
             list_tuple_all_poke.append((table_link_tuple[1][-20:], name, ability, tera, item, list_moves[0], list_moves[1], list_moves[2], list_moves[3]))
             #print(name, 'teratipo:', tera, 'mosse:', *list_moves, 'abilità:', ability, 'item held:', item)
         return list_tuple_all_poke
-    
-
-    
